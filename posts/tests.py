d6a8ba613751033a659486b142cb100f83d6944e
Nimbus <210294428+Nimbus-commints@users.noreply.github.com>
2025-11-08 11:30:05 -0500

    added some tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase  # TO test Databases
+from django.urls import reverse
+from .models import Post  # added
 
 # Create your tests here.
+
+
+class PostTests(TestCase):  # added
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
+
+    # url exist, name available, template correct, content
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/posts/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get(reverse("posts"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "posts.html")
+        self.assertContains(response, "This is a test!")
+
+
+# better to use the test_homepage(self)
+# def test_url_available_by_name(self):
+#     response = self.client.get(reverse("posts"))
+#     self.assertEqual(response.status_code, 200)
+
+# def test_template_name_correct(self):
+#     response = self.client.get(reverse("posts"))
+#     self.assertTemplateUsed(response, "posts.html")
+
+# def test_template_content(self):
+#     response = self.client.get(reverse("posts"))
+#     self.assertContains(response, "This is a test!")
